bc74da140852098afc10b30d24dabb5afe86f1dc
unknown <Rishabh.Trivedi@NOD-AF1-LO-ARTE.timesgroup.com>
2015-04-16 12:58:02 +0530

    changes

--- app/views/venue/index.volt
+++ app/views/venue/index.volt
@@ -5,16 +5,44 @@
 			<div class="col-sm-12 col-md-6">
 				<h2 class="venuetitle">{{venuedetail['title']}}</h2>
 				<div class="venuedetail">
-					<div class="time"><i class="fa fa-map-marker"></i>{{venuedetail['formatted_address']}}</div>
-					<div class="phone"><i class="fa fa-phone"></i><?php print_r($venuedetail['phonedata']); ?>, <?php print_r($venuedetail['mobiledata']); ?></div>
+					<div class="time">{{venuedetail['formatted_address']}}</div>
+					<div class="phone"><?php echo implode(',', $venuedetail['phonedata']); if($venuedetail['phonedata']){echo ",";}?> <?php echo implode(',', $venuedetail['mobiledata']); ?></div>
 					<div class="landmark">{{venuedetail['landmark']}}</div>
 					<div class="website"><a href="{{venuedetail['website']}}" target="_blank">{{venuedetail['website']}}</a></div>
 				</div>
 			</div>
 			<div class="col-sm-12 col-md-6">
-				
+				<div id="map_canvas" style="width: 100%; height: 250px;"></div>
 			</div>
 			<hr>
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+//<![CDATA[
+ 
+var map;
+ 
+// Ban Jelačić Square - Center of Zagreb, Croatia
+var center = new google.maps.LatLng(45.812897, 15.97706);
+ 
+function init() {
+	 
+	var mapOptions = {
+		zoom: 13,
+		center: center,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	 
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	 
+	var marker = new google.maps.Marker({
+		map: map, 
+		position: center,
+	});
+}
+//]]>
+init();
+</script>
--- cache/volt/c%%%%xampp%%htdocs%%phalcon%%app%%views%%venue%%index.volt.php
+++ cache/volt/c%%%%xampp%%htdocs%%phalcon%%app%%views%%venue%%index.volt.php
@@ -5,16 +5,44 @@
 			<div class="col-sm-12 col-md-6">
 				<h2 class="venuetitle"><?php echo $venuedetail['title']; ?></h2>
 				<div class="venuedetail">
-					<div class="time"><i class="fa fa-map-marker"></i><?php echo $venuedetail['formatted_address']; ?></div>
-					<div class="phone"><?php print_r($venuedetail['phonedata']); ?>, <?php print_r($venuedetail['mobiledata']); ?></div>
+					<div class="time"><?php echo $venuedetail['formatted_address']; ?></div>
+					<div class="phone"><?php echo implode(',', $venuedetail['phonedata']); if($venuedetail['phonedata']){echo ",";}?> <?php echo implode(',', $venuedetail['mobiledata']); ?></div>
 					<div class="landmark"><?php echo $venuedetail['landmark']; ?></div>
 					<div class="website"><a href="<?php echo $venuedetail['website']; ?>" target="_blank"><?php echo $venuedetail['website']; ?></a></div>
 				</div>
 			</div>
 			<div class="col-sm-12 col-md-6">
-				
+				<div id="map_canvas" style="width: 100%; height: 250px;"></div>
 			</div>
 			<hr>
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+//<![CDATA[
+ 
+var map;
+ 
+// Ban Jelačić Square - Center of Zagreb, Croatia
+var center = new google.maps.LatLng(45.812897, 15.97706);
+ 
+function init() {
+	 
+	var mapOptions = {
+		zoom: 13,
+		center: center,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	 
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	 
+	var marker = new google.maps.Marker({
+		map: map, 
+		position: center,
+	});
+}
+//]]>
+init();
+</script>
