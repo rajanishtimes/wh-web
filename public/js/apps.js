b3bb081c4ddd8c4b174c3b24fd1d37126864a2fb
unknown <Rishabh.Trivedi@NOD-AF1-LO-ARTE.timesgroup.com>
2015-05-06 11:21:42 +0530

    changes for dime

--- public/js/apps.js
+++ public/js/apps.js
@@ -173,16 +173,16 @@ function ajax_feed_filter_type(){
 
 function resizefeedimage(){
 	var width = 0;
-	/* $(".work-item img").each(function( index ) {
+	$(".work-item img").each(function( index ) {
 		if($(this).parent().find('.make-up').length == 0){
 			width = $(this).width();
 		}
 		$(this).height(($(this).width()));
-	}); */
+	});
 	
-	/* $(".withmask").each(function( index ) {
-		$(this).height(width+149);
-	}); */
+	$(".withmask").each(function( index ) {
+		$(this).height(width+147);
+	}); 
 }
 
 $.fn.center = function () {
