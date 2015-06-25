c6a0a262895dbafbe1d3b9a7909ba47f3c4902d1
System Administrator <root@NOD-BF2-MB-D160.local>
2015-06-25 16:03:03 +0530

    changes in ads

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -100,7 +100,7 @@ class SearchController extends BaseController{
 		$this->view->entitytype = 'search';
 		
 		$start = 0;
-		$limit = 1;
+		$limit = 12;
 		
 		try{
 			$allfeedslist = $this->getfeeddata($start, $limit, $this->currentCity, '', '', $searchkeyword, '', '', 'search');
