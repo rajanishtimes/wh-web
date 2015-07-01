e690beb595f29b42bfa5d042eff7d0ec6349ff16
System Administrator <root@NOD-BF2-MB-D160.local>
2015-07-01 20:23:52 +0530

    nomination list added

--- app/controllers/LogController.php
+++ app/controllers/LogController.php
@@ -23,14 +23,14 @@ class LogController extends BaseController{
 		exit;
     }
 
-
+48762
     public function nominationAction(){
     	$connection = mysql_connect('192.169.34.185', 'fireBird', 'FHW%aw1') or die('Could not connect to server.');
 		mysql_select_db('whweb', $connection) or die('Could not select database.');
 
 		mysql_query("delete from bnh_nominations");
 
-    	$haleem = array(19300, 30026, 139121, 50139, 48828, 48762, 42112, 47856, 38909, 36813, 45224, 50926, 44694, 46527, 33138, 43159, 27295, 7150, 42359);
+    	$haleem = array(19300, 30026, 139121, 50139, 48828, 48762, 42112, 47856, 38909, 36813, 30154, 45224, 50926, 44694, 46527, 33138, 43159, 27295, 7150, 42359);
 		$biryani = array(19300, 50926, 48828, 33487, 42112, 50139, 38909, 36813, 41605, 139120, 50970, 46968, 46527, 44694, 32052, 43159, 5778, 6121, 22636, 34289);
 
 		foreach($haleem as $haleemdata){
@@ -48,7 +48,7 @@ class LogController extends BaseController{
 			echo $query = "INSERT INTO bnh_nominations
 						SET contest_name = 'biryani and haleem',
 						contest = 'haleem',
-						title = '".$venuedetail['title']."',
+						title = '".stripcslashes($venuedetail['title'])."',
 						entity_id = ".$haleemdata.",
 						entity_type_id = 200,
 						url = '".$venuedetail['url']."',
@@ -78,7 +78,7 @@ class LogController extends BaseController{
 			echo $query = "INSERT INTO bnh_nominations
 						SET contest_name = 'biryani and haleem',
 						contest = 'biryani',
-						title = '".$venuedetail['title']."',
+						title = '".stripcslashes($venuedetail['title'])."',
 						entity_id = ".$haleemdata.",
 						entity_type_id = 200,
 						url = '".$venuedetail['url']."',
