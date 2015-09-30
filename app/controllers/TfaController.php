1249c44f861102c5f5dc6730227c0c8dfe94f29d
System Administrator <root@salonisssiphone.timesgroup.com>
2015-09-30 16:22:03 +0530

    changes in TFA

--- app/controllers/TfaController.php
+++ app/controllers/TfaController.php
@@ -15,4 +15,12 @@ class TfaController extends BaseController{
 		$this->view->meta_description = 'Times Food Awards & Times Nightlife Awards 2016 '.$this->cityshown($this->currentCity).': Find best restaurants, bars & clubs in '.$this->currentCity.'. Best dining and party places in '.$this->currentCity;
 		$this->view->meta_keywords = 'Times Food Awards, Times Nightlife Awards, Times Food Awards '.$this->cityshown($this->currentCity).', Times Nightlife Awards '.$this->currentCity;
     }
+
+    public function nominationAction(){
+		$title = $this->cityshown($this->currentCity).' Times Food & Nightlife Awards 2016, Best Restaurants | '.$this->config->application->SiteName;
+		$this->tag->setTitle($title);
+		$this->view->meta_description = 'Times Food Awards & Times Nightlife Awards 2016 '.$this->cityshown($this->currentCity).': Find best restaurants, bars & clubs in '.$this->currentCity.'. Best dining and party places in '.$this->currentCity;
+		$this->view->meta_keywords = 'Times Food Awards, Times Nightlife Awards, Times Food Awards '.$this->cityshown($this->currentCity).', Times Nightlife Awards '.$this->currentCity;
+		
+    }
 }
--- app/views/tfa/index.volt
+++ app/views/tfa/index.volt
@@ -1,25 +1,3 @@
-<section class="hp tfa">
-	<div class="mainback">
-		<div class="bordered">
-			<div class="desc">
-				<!--<div class="contestlogo"><img src="{{baseUrl}}/img/tfa/groupp.png"></div>-->
-
-				<div class="row">
-					<ul class="list logos padding0">
-						<li><img src="{{baseUrl}}/img/tfa/tfalogo.png"></li>
-						<li><img src="{{baseUrl}}/img/tfa/times_nightlife_awards.png"></li>
-					</ul>
-				</div>
-
-				<div class="presents">Powered by</div>
-				<div class="wh_logo"><img src="{{baseUrl}}/img/wh-logo-revert.png"></div>
-				<div class="coming_text">COMING SOON</div>
-				<!--<a href="#" class="scroll-down img-circle addscroll"><i class="fa fa-angle-down"></i></a>-->
-			</div>
-		</div>
-	</div>
-</section>
-
 <div class="clearfix"></div>
 <!--<div class="section">
 	<div class="container">
